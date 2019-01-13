8822bf1213b92fbbbbb13e61bdb6726952377a24
Radenko Kokanovic <radenko.kokanovic@gmail.com>
2019-01-13 12:29:46 +0100

    Develop branch add changes and css

--- index.php
+++ index.php
@@ -1,12 +1,54 @@
 <!DOCTYPE HTML>
 <html>  
+<head>
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+<!-- my Custom css -->
+<link rel="stylesheet" type="text/css" href="css/style.css">
+
+
+</head>
 <body>
 
+
+<!--
 <form action="welcome.php" method="post">
 Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
 <input type="submit">
 </form>
+
+ -->
+<h1>Form Handling</h1>
+<form action="welcome.php" method="post">
+  <div class="form-group">
+    <label for="text">Name:</label>
+    <input type="text" name="name" class="form-control" id="name">
+  </div>
+  <div class="form-group">
+    <label for="email">Email:</label>
+    <input type="email" name="email" class="form-control" id="email">
+  </div>
+  <div class="form-group form-check">
+    <label class="form-check-label">
+      <input class="form-check-input" type="checkbox"> Remember me
+    </label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+
+
 
 </body>
 </html>
